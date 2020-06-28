ae950fdac67d72ef4b4852e54a4c02435d8460c9
EitanFuturo <llovia@gmail.com>
2020-06-28 20:27:06 -0300

    Fixed connection to channel and added functionality.

--- bot.rb
+++ bot.rb
@@ -6,22 +6,21 @@ TWITCH_HOST = "irc.chat.twitch.tv"
 TWITCH_PORT = 6667
 
 class TwitchBot
-  
   def initialize
     @nickname = "futubot"
     @password = ENV['TWITCH_PASSWORD']
     @channel = "beginbot"
     @socket = TCPSocket.open(TWITCH_HOST, TWITCH_PORT)
-    
+
     write_to_system "PASS #{@password}"
-    write_to_system "NICK #{@nickname}"
-    write_to_system "JOIN #{@channel}"
+      write_to_system "NICK #{@nickname}"
+      write_to_system "JOIN ##{@channel}"
+    end
+
+    def write_to_system(message)
+    @socket.puts message
   end
 
-  def write_to_system(message)
-    @socket.puts message 
-  end
-  
   def write_to_chat(message)
     write_to_system "PRIVMSG ##{@channel} :#{message}"
   end
@@ -31,22 +30,28 @@ class TwitchBot
       message = @socket.gets
       puts message
 
+      if message.match(/^PING :(.*)$/)
+        write_to_system "PONG #{$~[1]}"
+        next
+      end
+
       if message.match(/PRIVMSG ##{@channel} :(.*)$/)
         content = $~[1]
         username = message.match(/@(.*).tmi.twitch.tv/)[1]
-        
-        if content.include? "hola"
+
+        if content.include? "!!hola"
           write_to_chat "hola #{username}"
         end
 
-        puts "new message"
-        puts "#{content} #{username}"
+        if content.include? "!!give"
+          write_to_chat "!props"
+        end
       end
     end
+  end
 
+  def quit
+    write_to_system "PART ##{@channel}"
+    write_to_system "QUIT"
   end
 end
-
-#bot = TwitchBot.new
-#bot.run
-
